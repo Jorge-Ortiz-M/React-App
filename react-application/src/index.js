1a378b008022752d44c7c2b9943eb558a7c64f44
Jorge-Ortiz-M <jorge.ortiz@icalialabs.com>
2022-05-06 10:13:28 -0500

    Adding variables to the react app.

--- react-application/src/index.js
+++ react-application/src/index.js
@@ -2,16 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const first_name = "Jorge";
+const last_name = "Ortiz";
+const position = "Software engineer";
+const company = "Icalia Labs"
+const hobbies = ['Videogames', 'Soccer', 'Teaching']
+
 ReactDOM.render(
     <div>
-        <h1>Jorge Ortiz</h1>
-        <p>Software engineer at Icalia Labs.</p>
+        <h1>Hello new world, My name is: {first_name} {last_name}.</h1>
+        <p>I'm a {position} at {company}.</p>
         <h2>Skills</h2>
         <ul>
             <li>Ruby on Rails</li>
             <li>Ruby</li>
             <li>Javascript</li>
             <li>PostgreSQL.</li>
+        </ul>
+        <h2>Hobbies</h2>
+        <ul>
+            <li>{hobbies[0]}</li>
+            <li>{hobbies[1]}</li>
+            <li>{hobbies[2]}</li>
         </ul>
     </div>
     , document.getElementById("root"));
