23d0f96575e6dba6e2faea3914188373fabf886d
Jorge-Ortiz-M <jorge.ortiz@icalialabs.com>
2022-05-06 10:03:28 -0500

    Adding a short list with three points.

--- react-application/src/index.js
+++ react-application/src/index.js
@@ -6,5 +6,12 @@ ReactDOM.render(
     <div>
         <h1>Jorge Ortiz</h1>
         <p>Software engineer at Icalia Labs.</p>
+        <h2>Skills</h2>
+        <ul>
+            <li>Ruby on Rails</li>
+            <li>Ruby</li>
+            <li>Javascript</li>
+            <li>PostgreSQL.</li>
+        </ul>
     </div>
     , document.getElementById("root"));
