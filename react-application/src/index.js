715d2ab9dbdafd0bb9465fbc0c2917921fd92941
Jorge-Ortiz-M <jorge.ortiz@icalialabs.com>
2022-05-05 23:40:19 -0500

    Showing a simple message.

--- react-application/src/index.js
+++ react-application/src/index.js
@@ -1,3 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import './index.css';
 
+ReactDOM.render(
+    <div>
+        <h1>Jorge Ortiz</h1>
+        <p>Software engineer at Icalia Labs.</p>
+    </div>
+    , document.getElementById("root"));
